81e6cad0ce5672e6c5fe9ed51cd7d97101facad7
Takahiro Watanabe <Tak0002@users.noreply.github.com>
2021-04-18 23:44:44 +0900

    Update script.js

--- examples/room/script.js
+++ examples/room/script.js
@@ -41,7 +41,7 @@ const Peer = window.Peer;
 
   // eslint-disable-next-line require-atomic-updates
   const peer = (window.peer = new Peer({
-    key: window.__SKYWAY_KEY__,
+    key: "5bea388b-3f95-4e1e-acb5-a34efdd0c480",
     debug: 3,
   }));
 
